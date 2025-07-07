70f326deb922e4212927d722fe7e8a2037c7e2d0
Mohita Shrivastava <mohita_shrivastava@epam.com>
2025-07-07 15:40:30 +0530

    Front End Completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { ContactPageComponent } from './contact-page/contact-page.component';
 import { MyListingsPageComponent } from './my-listings-page/my-listings-page.component';
 import { NewListingPageComponent } from './new-listing-page/new-listing-page.component';
 import { EditListingPageComponent } from './edit-listing-page/edit-listing-page.component';
+import { ListingDataFormComponent } from './listing-data-form/listing-data-form.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import { EditListingPageComponent } from './edit-listing-page/edit-listing-page.
     ContactPageComponent,
     MyListingsPageComponent,
     NewListingPageComponent,
-    EditListingPageComponent
+    EditListingPageComponent,
+    ListingDataFormComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
--- src/app/edit-listing-page/edit-listing-page.component.ts
+++ src/app/edit-listing-page/edit-listing-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Listing } from '../types';
+import { fakeMyListings } from '../fake-data';
+
 
 @Component({
   selector: 'app-edit-listing-page',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-listing-page.component.css']
 })
 export class EditListingPageComponent implements OnInit {
+  listing: Listing
+  name: string = ''
+  description: string = ''
+  price: string = '';
 
-  constructor() { }
+  constructor(private router: Router,
+      private activeroute:ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    const id = this.activeroute.snapshot.paramMap.get('id');
+    this.listing = fakeMyListings.find(list => list.id === id)
+    this.name = this.listing.name
+    this.description = this.listing.description;
+    this.price = this.listing.price.toString();
   }
 
+  onSubmit() {
+    alert('Saving changes to the listing....')
+    this.router.navigateByUrl('/my-listings')
+  }
 }
